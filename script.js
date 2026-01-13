e1969e34b5b459cd8ae6c51bce6f6baf45e6703d
bhaveshkhose <khosebhavesh@gmail.com>
2026-01-14 00:21:30 +0530

    Backend Implementation with frontEnd

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('theme', newTheme);
   });
 
-  
+  renderProject();
 });
 
 const projects = [
@@ -33,14 +33,14 @@ const projects = [
 {
   title : "Expense Tracking System",
   description : "This Expense Tracker Web Application helps users efficiently monitor and manage their daily expenses by categorizing transactions and providing clear financial insights. Built using Spring Boot, REST APIs, relational databases, and a modern frontend stack, it ensures secure data handling and smooth user interaction. The project solves the problem of unorganized expense tracking by offering a centralized, user-friendly platform for better financial control and decision-making.",
-  imageUrl:"images/prroject3.png",
+  ImageUrl:"images/prroject3.png",
   liveUrl : "#",
   codeUrl :"https://github.com/bhaveshkhose/x_track"
 },
 {
   title : "Business Management System",
   description : "Developed a Business Management System using HTML, CSS, JavaScript, Spring Boot, and MySQL to streamline core business operations such as data management, process tracking, and reporting. The system provides a secure, user-friendly interface with a robust backend to automate workflows, reduce manual effort, and improve overall efficiency and decision-making",
-  imageUrl:"images/project4.png",
+  ImageUrl:"images/project4.png",
   liveUrl : "#",
   codeUrl :"https://github.com/bhaveshkhose/x_track"
 }
@@ -77,4 +77,44 @@ const renderProject = () => {
 }
 
 renderProject();
+
+
+document.getElementById("contactForm").addEventListener("submit" , async(e) =>{
+  e.preventDefault();
+
+  const data = {
+      name : document.getElementById("name").value,
+      email : document.getElementById("email").value,
+      message : document.getElementById("message").value
+  };
+
+  const response = await fetch("http://localhost:8080/api/message", {
+
+    method:"post",
+    headers :{"Content-type" : "application/json"},
+    body:JSON.stringify(data)
+
+  });
+
+  if(response.ok){
+    alert("Message sent Successfully..!");
+  }else{
+    alert("Something went wrong...");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
